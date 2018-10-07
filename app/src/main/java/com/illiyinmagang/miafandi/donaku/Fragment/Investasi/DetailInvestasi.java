5d9352dc08188172c32a7355ff96f5d55ab8d6d7
irwanafandi24 <mohamadirwanafandi@gmail.com>
2018-10-07 14:55:59 +0700

    investasi

---
+++ app/src/main/java/com/illiyinmagang/miafandi/donaku/Fragment/Investasi/DetailInvestasi.java
@@ -0,0 +1,15 @@
+package com.illiyinmagang.miafandi.donaku.Fragment.Investasi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.illiyinmagang.miafandi.donaku.R;
+
+public class DetailInvestasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_investasi);
+    }
+}
