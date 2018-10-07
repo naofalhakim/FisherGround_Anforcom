15894a8a90159709b6686dc003a6e8d7fdda2057
irwanafandi24 <mohamadirwanafandi@gmail.com>
2018-10-07 23:55:42 +0700

    last

--- app/src/main/java/com/illiyinmagang/miafandi/donaku/Fragment/HomeUser/BayarActivity.java
+++ app/src/main/java/com/illiyinmagang/miafandi/donaku/Fragment/HomeUser/BayarActivity.java
@@ -1,13 +1,17 @@
 package com.illiyinmagang.miafandi.donaku.Fragment.HomeUser;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.illiyinmagang.miafandi.donaku.HomeUserActivity;
 import com.illiyinmagang.miafandi.donaku.R;
 
 public class BayarActivity extends AppCompatActivity {
-
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +20,14 @@ public class BayarActivity extends AppCompatActivity {
         //back botton
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btnSubmit = (Button) findViewById(R.id.btnBayar);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(BayarActivity.this, HomeUserActivity.class));
+            }
+        });
     }
 
     @Override
